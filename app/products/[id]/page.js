e7230f42cb8821abe0cfe6247bec830bb3803f06
smhdes <desdemir.semih@gmail.com>
2025-10-04 23:47:13 +0300

    Fix generateStaticParams and use client conflict: Move 'use client' after generateStaticParams

--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -1,5 +1,3 @@
-'use client';
-
 import { productAPI } from '../../../lib/api';
 
 // Generate static params for all products
@@ -14,6 +12,8 @@ export async function generateStaticParams() {
     return [];
   }
 }
+
+'use client';
 
 import { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
